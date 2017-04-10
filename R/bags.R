159ba80d7454d37b3b7efe90ac5ee7cf94b06cbd
Thomas Mailund <mailund@birc.au.dk>
2017-04-10 16:07:20 +0200

    tree bags

--- R/bags.R
+++ R/bags.R
@@ -31,3 +31,70 @@ merge.list_bag <- function(x, y) {
   result
 }
 
+
+
+
+#' Add a head item to a tree implementation of a bag.
+#' @param elem  The item to put at the head of the bag
+#' @param bag   The bag -- it will become the tail of the new list.
+#' @return a new bag.
+#' @export
+bag_node <- function(elem, left, right)
+  structure(list(item = elem, left = left, right = right),
+            class = "tree_bag")
+
+tree_bag_nil <- bag_node(NA, NULL, NULL)
+
+#' @method is_empty tree_bag
+#' @export
+is_empty.tree_bag <- function(x) identical(x, tree_bag_nil)
+
+#' Create an empty tree bag.
+#' @return an empty tree bag.
+#' @export
+empty_tree_bag <- function() tree_bag_nil
+
+#' @method insert tree_bag
+#' @export
+insert.tree_bag <- function(x, elm, ...) {
+  element_leaf <- bag_node(elm, empty_tree_bag(), empty_tree_bag())
+  if (is_empty(x)) element_leaf
+  else bag_node(NA, element_leaf, x)
+}
+
+#' @method merge tree_bag
+#' @export
+merge.tree_bag <- function(x, y) {
+  bag_node(NA, x, y)
+}
+
+is_leaf <- function(x) {
+  is_empty(x$left) && is_empty(x$right)
+}
+bag_to_list <- function(x, acc) {
+  if (is_leaf(x)) list_cons(x$item, acc)
+  else bag_to_list(x$right, bag_to_list(x$left, acc))
+}
+
+#' Translate a tree bag into an R list.
+#'
+#' @param x The tree bag
+#' @param ... Additional parameters. Not used but part of the generic interface.
+#' @return An R list with the elements found in the linked list.
+#' @method as.list tree_bag
+#' @export
+as.list.tree_bag <- function(x, ...) {
+  as.list(bag_to_list(x, empty_list()))
+}
+
+#' Translate a tree bag into an R vector.
+#'
+#' @param x    The tree bag.
+#' @param mode The mode (numeric, logical, etc.) for the constructed vector.
+#'             The default will be a list
+#' @return An R vector with the elements found in the bag.
+#' @method as.vector tree_bag
+#' @export
+as.vector.tree_bag <- function(x, mode = "any") {
+  as.vector(as.list(x), mode)
+}
--- tests/testthat/test_bags.R
+++ tests/testthat/test_bags.R
@@ -8,6 +8,21 @@ test_that("We can construct linked lists bag correctly", {
 test_that("We can concatenate two list bags", {
   bag1 <- bag_cons(1, bag_cons(2, bag_cons(3, empty_list_bag())))
   bag2 <- bag_cons(4, bag_cons(5, bag_cons(6, empty_list_bag())))
-  bag <- list_concatenate(bag1, bag2)
+  bag <- merge(bag1, bag2)
+  expect_equal(as.vector(bag, "integer"), 1:6)
+})
+
+test_that("We can construct tree bag correctly", {
+  bag <- empty_tree_bag()
+  for (x in 1:3) bag <- insert(bag, x)
+  expect_equal(as.vector(bag, "integer"), 1:3)
+})
+
+test_that("We can concatenate two list bags", {
+  bag1 <- empty_tree_bag()
+  for (x in 1:3) bag1 <- insert(bag1, x)
+  bag2 <- empty_tree_bag()
+  for (x in 4:6) bag2 <- insert(bag2, x)
+  bag <- merge(bag2, bag1) # notice order...
   expect_equal(as.vector(bag, "integer"), 1:6)
 })
